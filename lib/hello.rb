04c52683c3ab8b0902c6b04008938b92d4729a6c
mach1010 <jetstream1010@gmail.com>
2014-04-04 18:45:09 -0500

    made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,4 @@
-#Default is 'World'
-#Author: mach1010 (mach1010@github.com)
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
